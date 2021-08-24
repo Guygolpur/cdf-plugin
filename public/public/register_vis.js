e20636d2e094b39d362a89466168465676d28055
INTL\gg341t <ggolpur@kpmg.com>
2021-08-24 18:58:25 +0300

    removed public inner folder.

--- public/public/register_vis.js
+++
@@ -1,4 +0,0 @@
-import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
-import CdfLineVisTypeFactory from 'plugins/arc_cdf_line_vis/cdf_line';
-
-VisTypesRegistryProvider.register(CdfLineVisTypeFactory);
