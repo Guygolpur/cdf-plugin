11f56c5a6001d9d72df924b1f5884790b75c7e9d
ggolpur <im341t@intl.att.com>
2021-10-03 18:50:24 +0300

    added listener to subBucketArray.

--- public/cdf_vis/cdf_component.tsx
+++ public/cdf_vis/cdf_component.tsx
@@ -179,6 +179,7 @@ export function CdfComponent(props: CdfComponentProps) {
     dateFilterTo,
     splitedHistogramMinInterval,
     splitedDateHistogramMinInterval,
+    subBucketArray
   ]);
 
   return (
